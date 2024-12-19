67434f2c96bb54be3dc137553f6bf6850b0fc43b
StuxieDev <contact@stuxie.dev>
2024-12-19 11:53:57 +0000

    Initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <Navbar></Navbar>
+        <v-main>
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    // import Index from './components/Index'
+    import Navbar from "./components/Navbar/Navbar";
+
+    export default {
+        name: 'App',
+
+        components: {
+            Navbar,
+            // Index
+        },
+
+        data: () => ({
+            //
+        })
+    }
+</script>
