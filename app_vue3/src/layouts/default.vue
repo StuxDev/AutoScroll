ef6ffb120f05c6c132ac93f26b04574e0965cd96
Leo "StuxieDev" Ridgwell <contact@stuxie.dev>
2026-03-21 08:48:40 +0000

    Add anchor color styling to links in footer for consistency

--- app_vue3/src/layouts/default.vue
+++ app_vue3/src/layouts/default.vue
@@ -54,6 +54,7 @@
               This is a small side project made during 2024. I wanted to take the time to learn more about Vue.js and Vuetify. I was inspired by <a
                 href="http://www.imagoid.com/"
                 target="_blank"
+                style="color: var(--v-theme-anchor)"
               >Imagoid</a>
               and so created a very simple auto scroller for Reddit image posts. I hope you enjoy, and if you find any issues you can leave an issue in the source code repository!
               <v-icon>mdi-emoticon-happy</v-icon>
@@ -62,6 +63,7 @@
               You can find me on my links page <a
                 href="https://links.stuxie.dev"
                 target="_blank"
+                style="color: var(--v-theme-anchor)"
               >here</a> for any further questions or inquiries. Stay safe!
 
               <br><br>
